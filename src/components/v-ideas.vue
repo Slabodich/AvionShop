6bf1d567c4b17b828f90ab2eeb93c59fc4496413
Georgiy <egorslabodich1@gmail.com>
2022-12-16 12:08:25 +0300

    Add few component and routing

---
+++ src/components/v-ideas.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "v-ideas"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-informationForBlock.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "v-informationForBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
