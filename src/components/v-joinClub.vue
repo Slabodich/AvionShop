007c3a1216e2acc277d118f8daf5f9849c1b50f2
Georgiy <egorslabodich1@gmail.com>
2022-12-19 13:06:55 +0300

    Add footer

---
+++ src/components/v-joinClub.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "v-joinClub"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-subscribeForm.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "v-subscribeForm"
+}
+</script>
+
+<style scoped>
+
+</style>
